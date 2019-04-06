ce2bc78e6cb4e4ff45bc24ebdc55332ad72236e6
Kimberly Nicholls <kimberly@gridium.com>
2019-04-06 10:39:45 -0700

    pylint

--- index.py
+++ index.py
@@ -17,26 +17,28 @@ parser.add_argument('--port', dest='port', type=int, default=8000, help='port fo
 args = parser.parse_args()
 
 
-def read_json(fn, prefix):
-    with open(fn) as f:
+def read_json(filename, prefix):
+    """read file fn as JSON; remove path prefix with """
+    with open(filename) as inf:
         # // and /* */ comments aren't valid JSON
-        data = re.sub(r'//.*', '', f.read())
+        data = re.sub(r'//.*', '', inf.read())
         data = re.sub(r'/\*.*?\*/', '', data, flags=re.DOTALL)
         try:
             obj = json.loads(data)
             # relative path
-            path = re.sub(prefix, '', fn).replace('\\', '/')
+            path = re.sub(prefix, '', filename).replace('\\', '/')
             obj['full_path'] = path
             parts = path.split('/')
             obj['path'] = '/'.join(parts[:-1])
             return obj
-        except json.decoder.JSONDecodeError as e:
-            print('error parsing %s' % (fn))
+        except json.decoder.JSONDecodeError:
+            print('error parsing %s' % (filename))
             traceback.print_exc()
             return None
 
 
 def collect_keys(props, obj):
+    """get keys from object and set name, type, objects, and values in props"""
     for k in obj.keys():
         data_type = re.match(r".*?'(.*?)'>", str(type(obj[k]))).group(1)
         props.setdefault(k, {
@@ -59,8 +61,8 @@ def collect_keys(props, obj):
             props[k]['values'].add(str(obj[k]))
 
 
-# find object files
 def read_objects(prefix):
+    """read files under _prefix_/objects and load into dict"""
     path = '%s/objects' % prefix
     object_files = glob.glob('%s/**/*.object' % path, recursive=True)
     print('%s objects in %s' % (len(object_files), path))
@@ -68,8 +70,8 @@ def read_objects(prefix):
     props = {}
     effects = {}
     idx = 0
-    for fn in object_files:
-        obj = read_json(fn, prefix)
+    for filename in object_files:
+        obj = read_json(filename, prefix)
         idx += 1
         if not idx % 100:
             print('%s/%s' % (idx, len(object_files)))
@@ -88,14 +90,15 @@ def read_objects(prefix):
 
 
 def read_status_effects(prefix):
+    """read files under _prefix_/stats/effects and load into dict"""
     path = '%s/stats/effects' % prefix
     effect_files = glob.glob('%s/**/*.statuseffect' % path, recursive=True)
     print('%s effects in %s' % (len(effect_files), path))
     effects = {}
     props = {}
     idx = 0
-    for fn in effect_files:
-        obj = read_json(fn, prefix)
+    for filename in effect_files:
+        obj = read_json(filename, prefix)
         idx += 1
         if not idx % 100:
             print('%s/%s' % (idx, len(effect_files)))
@@ -108,19 +111,22 @@ def read_status_effects(prefix):
     }
 
 
-def read_template(fn):
-    with open('templates/%s.jinja2' % fn) as f:
-        return Template(f.read())
+def read_template(filename):
+    """read template file and return contents"""
+    with open('templates/%s.jinja2' % filename) as inf:
+        return Template(inf.read())
 
 
 def write_file(out_fn, templates, context):
-    with open(out_fn, 'w') as f:
+    """merge templates with content and write to out_fn"""
+    with open(out_fn, 'w') as outf:
         for template in templates:
-            f.write(template.render(context))
+            outf.write(template.render(context))
     #print('wrote %s' % out_fn)
 
 
 def exclude_meta(src_obj):
+    """return copy of src_obj, without path, img, full_path, and objects keys"""
     obj = copy.copy(src_obj)
     for key in ['path', 'img', 'full_path', 'objects']:
         if key in obj:
@@ -129,13 +135,14 @@ def exclude_meta(src_obj):
 
 
 def write_props(key, props, out_path, header, footer):
+    """read template key, merge with props, and write to out_path with header/ZAfooter"""
     body = read_template(key)
     props_copy = exclude_meta(props)
-    for p in props_copy:
-        vals = list(props[p]['values'])
+    for prop in props_copy:
+        vals = list(props[prop]['values'])
         vals.sort()
-        props[p]['values'] = vals[:10]
-        props[p]['value_count'] = len(vals)
+        props[prop]['values'] = vals[:10]
+        props[prop]['value_count'] = len(vals)
     context = {
         'active': key,
         'props': props_copy
@@ -144,6 +151,7 @@ def write_props(key, props, out_path, header, footer):
 
 
 def write_objects(obj_type, objects, out_path, header, footer):
+    """read template for obj_type, merge with objects, and write to out_path with header/footer"""
     body = read_template(obj_type)
     for key in objects:
         obj = exclude_meta(objects[key])
@@ -166,6 +174,7 @@ def write_objects(obj_type, objects, out_path, header, footer):
 
 
 def link_effects(objects, effects):
+    """get effects available for each object"""
     for obj_name in objects:
         obj = objects[obj_name]
         for eff_key in ['sitStatusEffects', 'statusEffects']:
@@ -182,7 +191,7 @@ def link_effects(objects, effects):
 
 
 def write_json(objects, effects, out_path):
-    # json data for search
+    """write json data for search"""
     out_fn = '%s/objects.json' % (out_path)
     obj_list = []
     for key in objects:
@@ -204,12 +213,13 @@ def write_json(objects, effects, out_path):
             'img': obj.get('icon'),
             'description': obj.get('label', '')
         })
-    with open(out_fn, 'w') as f:
-        f.write(json.dumps(obj_list))
+    with open(out_fn, 'w') as outf:
+        outf.write(json.dumps(obj_list))
     print('wrote %s' % out_fn)
 
 
 def main():
+    """read asset data and write as HTML/JSON"""
     obj = read_objects(args.path)
     effects = read_status_effects(args.path)
     link_effects(obj['objects'], effects['effects'])
@@ -234,11 +244,13 @@ def main():
     write_json(obj['objects'], effects['effects'], out_path)
     shutil.copy('templates/style.css', '%s/style.css' % out_path)
     shutil.copy('templates/app.js', '%s/app.js' % out_path)
-    write_file('%s/index.html' % out_path, [header, read_template('index'), footer],
-        {'objects': '{:,}'.format(len(obj['objects'])), 'effects': '{:,}'.format(len(effects['effects']))})
+    write_file('%s/index.html' % out_path, [header, read_template('index'), footer], {
+        'objects': '{:,}'.format(len(obj['objects'])),
+        'effects': '{:,}'.format(len(effects['effects']))
+    })
     os.chdir(args.path)
     with socketserver.TCPServer(('', args.port), http.server.SimpleHTTPRequestHandler) as httpd:
-        print('load http://localhost:%s/index.html in a browser' % args.port)
+        print('load http://localhost:%s/asset-doc/index.html in a browser' % args.port)
         httpd.serve_forever()
 
 if __name__== "__main__":
